ab7a93cb4f7531e01f2ba888b4cd6d0a5472c0e8
TianJingle <32089226+MyLegendLife@users.noreply.github.com>
2018-07-25 21:55:29 +0800

    Add

---
+++ Demo.DAL/Entities/Base/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entities.Base
+{
+    public class EntityBase
+    {
+        public EntityBase()
+        {
+
+        }
+
+        public String Id { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Demo.DAL/Entities/User/User.cs
@@ -0,0 +1,23 @@
+﻿using Demo.DAL.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entities.User
+{
+    public class User : EntityBase
+    {
+        public User()
+        {
+
+        }
+
+        public String Name { get; set; }
+
+        public String Tel { get; set; }
+
+        public Int32 Level { get; set; }
+    }
+}
